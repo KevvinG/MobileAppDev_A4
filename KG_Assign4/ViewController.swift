190c1fafff3ca2e1778144e1e188c6fb49696688
Kevin Grzela <kevvingrzela@gmail.com>
2019-11-12 18:22:34 -0500

    Initial Commit

---
+++ KG_Assign4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KG_Assign4
+//
+//  Created by Kevin Grzela on 2019-11-12.
+//  Copyright © 2019 Kevin Grzela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
